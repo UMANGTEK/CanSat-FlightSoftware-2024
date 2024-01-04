7d366ccc9a6ba3f0709c4c1e0f1f010d991d75ec
UMANGTEK <umangtekriwal1@gmail.com>
2024-01-05 04:12:55 +0530

    Made enum states so FSW

--- CanSat-FlightSoftware2024/Cansat-FlightSoftware2024.ino
+++ CanSat-FlightSoftware2024/Cansat-FlightSoftware2024.ino
@@ -1,3 +1,27 @@
+#define packetTimePeriod 1000
+#include <TimeLib.h>
+
+
+enum states {
+  IDLE ,
+  LAUNCH_WAIT ,
+  ASCENT ,
+  DECENT ,
+  PARA_NOSECONE_DEPLOYED ,
+  LANDED
+};
+enum modes {
+  FLIGHT,
+  SIMULATION
+};
+
+int currentState = IDLE ;
+int currentMode = FLIGHT ;
+int packet_count = 0;
+bool PARA_deployed = false;
+bool NOSE_RELEASED = false;
+
+
 void setup()
 {
 }

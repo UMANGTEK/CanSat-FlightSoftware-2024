899050d49e05c150102b565f2d67d65ee027e9e7
UMANGTEK <umangtekriwal1@gmail.com>
2024-01-05 20:06:45 +0530

    FSW included some backup files

--- CanSat-FlightSoftware2024/Cansat-FlightSoftware2024.ino
+++ CanSat-FlightSoftware2024/Cansat-FlightSoftware2024.ino
@@ -1,6 +1,11 @@
 #define packetTimePeriod 1000
 #include <TimeLib.h>
 
+#include "servo.h"
+#include "eeprom_rw.h"
+#include "led_buzzer.h"
+#include "telemetry.h"
+#include "smartDelay.h"
 
 enum states {
   IDLE ,

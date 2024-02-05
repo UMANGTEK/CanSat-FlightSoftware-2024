81b8cd8e0c296a4442cfd48cd3e38e43b03e09f8
genneutrino <dronpande154@gmail.com>
2024-02-05 11:44:35 +0530

    added FIR to code, still a WIP

--- CanSat-FlightSoftware2024/checkheight.h
+++ CanSat-FlightSoftware2024/checkheight.h
@@ -1,90 +1,120 @@
- #define MOVINGAVG 5
+#define MOVINGAVG 5
+#define FIR_TAP 3
 #define ERR_DIFF 1
 #define FRACTION 0.6
-float arr[MOVINGAVG]={0,0,0,0,0}; // add more zeros here if u are increasing MOVINGAVG
-/*
-void updateAlt(float alt  )
-{
-  for(int i=0;i<MOVINGAVG-1;i++){
-    arr[i] = arr[i+1];
-  }
-  arr[MOVINGAVG-1] = alt;
-}
-float getFiltered(){
-  int mean=0;
-  for(int i=0; i<MOVINGAVG; i++){
-    mean+= arr[i];
-  }
-  mean = mean/MOVINGAVG;
-  return ( mean );
-}
-*/
 #define ALPHA 0.9
+
+float arr[MOVINGAVG] = {0, 0, 0, 0, 0};
+float fir_coeffs[FIR_TAP] = {0.2, 0.3, 0.5};  // Adjust coefficients as needed
+float fir_buffer[FIR_TAP] = {0};
+
 float y = 0;
-void updateAlt(float alt  )
+
+void updateAlt(float alt)
 {
-  y = y * ( 1 - ALPHA ) + ALPHA * alt ;
-  
-  for(int i=0;i<MOVINGAVG-1;i++){
-    arr[i] = arr[i+1];
-  }
-  arr[MOVINGAVG-1] = alt;
-}
-float getFiltered(){
-  return(y);
+    // Exponential smoothing
+    y = y * (1 - ALPHA) + ALPHA * alt;
+
+    // Update circular buffer for moving average
+    for (int i = 0; i < MOVINGAVG - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[MOVINGAVG - 1] = alt;
+
+    // Update FIR filter buffer
+    for (int i = FIR_TAP - 1; i > 0; i--)
+    {
+        fir_buffer[i] = fir_buffer[i - 1];
+    }
+    fir_buffer[0] = alt;
 }
 
-bool checkAlt(float lessthanAlti) {
-  float mean = getFiltered();
-  if( mean > lessthanAlti  ){
-    return false;
-  }
-  else{
-    return true;
-  }
-}
-bool movingUp(){
-  int j = 0;
-  for(int i=1;i<MOVINGAVG ;i++){
-    if ( arr[i] > arr[i-1] ){
-      j++;
-    }
-  }
-  if ( j > FRACTION * MOVINGAVG ){
-    return true;
-  }
-  else {
-    return false;
-  }
+float getFiltered()
+{
+    return y;
 }
 
-bool movingDown(){
-  int j = 0;
-  for(int i=1;i<MOVINGAVG ;i++){
-    if ( arr[i] < arr[i-1] ){
-      j++;
+float getFIRFiltered()
+{
+    float result = 0;
+    for (int i = 0; i < FIR_TAP; i++)
+    {
+        result += fir_coeffs[i] * fir_buffer[i];
     }
-  }
-  if ( j > FRACTION*MOVINGAVG ){
-    return true;
-  }
-  else {
-    return false;
-  }
+    return result;
 }
 
-bool notMoving (){
-  int j = 0;
-  float filtered_alt = getFiltered();
-  for(int i=0;i<MOVINGAVG ;i++){
-    if ( arr[i] < filtered_alt + ERR_DIFF && arr[i] > filtered_alt - ERR_DIFF ){
-      j++;
+bool checkAlt(float lessthanAlti)
+{
+    float mean = getFiltered();
+    if (mean > lessthanAlti)
+    {
+        return false;
     }
-  }
-  if ( j == MOVINGAVG-1 ){
-    return true;
-  }
-  else {
-    return false;
-  }
+    else
+    {
+        return true;
+    }
+}
+
+bool movingUp()
+{
+    int j = 0;
+    for (int i = 1; i < MOVINGAVG; i++)
+    {
+        if (arr[i] > arr[i - 1])
+        {
+            j++;
+        }
+    }
+    if (j > FRACTION * MOVINGAVG)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool movingDown()
+{
+    int j = 0;
+    for (int i = 1; i < MOVINGAVG; i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            j++;
+        }
+    }
+    if (j > FRACTION * MOVINGAVG)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool notMoving()
+{
+    int j = 0;
+    float filtered_alt = getFiltered();
+    for (int i = 0; i < MOVINGAVG; i++)
+    {
+        if (arr[i] < filtered_alt + ERR_DIFF && arr[i] > filtered_alt - ERR_DIFF)
+        {
+            j++;
+        }
+    }
+    if (j == MOVINGAVG - 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
